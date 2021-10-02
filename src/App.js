8f9169274de0a64428f0281814620cc80b098532
Marvin Soto <marvin@yalochat.com>
2021-10-01 18:49:06 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <div className="main-header">
+        <h1 className="logo">
+          Pokechallenge
+        </h1>
+        <div className="main-search">
+          <form>
+            <input 
+                type="text"
+                name="poke_name"
+                placeholder="What is your favorite Pókemon?"
+            />
+          </form>
+        </div>
+        <a className="favorite-link" href="#">
+          Favorites
         </a>
-      </header>
-    </div>
-  );
+      </div>
+      <div className="pokemon-card">
+        <button className="material-icons favorite"
+            onClick = {() => {}}
+        >
+          favorite
+        </button>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png" alt="pokemon name"/>
+        <div>
+          <h1>
+            Charizard <span>No. 06</span>
+          </h1>
+          <p><strong>Height:</strong> <span>2 mts</span></p>
+          <p><strong>Weight:</strong> <span>100 kg</span></p>
+          <p><strong>Types:</strong></p>
+          <ul>
+            <li className="type flying">
+              Flying
+            </li>
+            <li className="type fire">
+              Fire
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
